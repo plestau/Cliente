dca3935b72adc183a41bdcedd671d9c34ce18c9e
Pablo Lestau <pablolestau@outlook.com>
2022-10-20 18:30:37 +0200

    Document object

---
+++ Tema 2/Worksheet 6/index.js
@@ -0,0 +1,11 @@
+// Ejercicio 2c (hecho con google.com)
+document.write("document.image.length");
+
+// Ejercicio 2d
+document.write("document.image[1]");
+
+// Ejercicio 2e
+document.write("document.links.length");
+
+// Ejercicio 2f
+document.write("document.title='insertar titulo nuevo'");
