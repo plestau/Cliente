5ba85c7938acff65fb2a2eab997f905e1a09f510
Pablo Lestau <pablolestau@outlook.com>
2022-10-20 18:23:30 +0200

    Basic exercises

---
+++ Tema 1/Worksheet 1/Ejercicio 4.js
@@ -0,0 +1,12 @@
+let var1 = 4;
+let var2 = 2;
+
+let suma = var1 + var2;
+let resta = var1 - var2;
+let mult = var1 * var2;
+let div = var1 / var2;
+
+alert("El valor de la suma es = "+suma);
+alert("El valor de la resta es = "+resta);
+alert("El valor de la multiplicación es = "+mult);
+alert("El valor de la división es = "+div);
