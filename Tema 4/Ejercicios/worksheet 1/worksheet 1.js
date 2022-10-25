536a0cae4ec298d3188800fe3452e023020f861a
Pablo Lestau <pablolestau@outlook.com>
2022-10-25 19:59:00 +0200

    saving changes on exercises

--- Tema 4/Ejercicios/worksheet 1/worksheet 1.js
+++ Tema 4/Ejercicios/worksheet 1/worksheet 1.js
@@ -7,5 +7,20 @@ function posicion(){
     $y = event.screenY;
     console.log("X: ",$x ," Y: ", $y);
 }
-
-
+  
+function dragLeave(event) {
+    if ( event.target.className == "droptarget" ) {
+        document.getElementById("demo").innerHTML = "Left the dropzone";
+        event.target.style.border = "";
+        alert("Funciona");
+    }
+}
+  
+function allowDrop(event) {
+    event.preventDefault();
+    
+}
+  
+function drop(event) {
+    alert("Funciona");
+}
