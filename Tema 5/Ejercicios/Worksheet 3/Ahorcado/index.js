ed87bdb4c31a2b51c1145299b3f850f5c530762b
Pablo Lestau <pablolestau@outlook.com>
2022-11-12 18:50:26 +0100

    Guardando cambios en Hangman

--- Tema 5/Ejercicios/Worksheet 3/Ahorcado/index.js
+++ Tema 5/Ejercicios/Worksheet 3/Ahorcado/index.js
@@ -1,26 +1,37 @@
 window.onload=function(){
-
     word = "Palabra";
+    n_vidas = 10;
     var wordArray = word.split("");
     var wordLength = wordArray.length;
     var resultado = document.getElementById("resultado");
+    var vidas = document.getElementById("vidas");
+    vidas.innerHTML = "Tienes " + n_vidas + " vidas";
     for (x=0; x<wordLength; x++) {
         resultado.innerHTML += " _ ";
     }
 
-    var button = document.getElementsByTagName("button");
+
+
+    
+    var button = document.querySelectorAll("button");
     for (i=0; i<button.length; i++) {
-        console.log(button[i]);
-        button[i].onclick = function() {
-            var letter = button.innerHTML;
-            var letterArray = letter.split("");
-            var letterLength = letterArray.length;
-            for (y=0; y<letterLength; y++) {
-                if (letterArray[y] == wordArray[y]) {
-                    resultado.innerHTML = letterArray[y];
+        button[i].addEventListener("click", function(){
+            var letra = this.innerHTML;
+            for (y=0; y<wordLength; y++) {
+                if (wordArray[y] == letra) {
+                    resultado.innerHTML = resultado.innerHTML.replaceAt(y*2, letra);
                 }
             }
-        }
-    }
-
+            if (resultado.innerHTML.indexOf(" _ ") == -1) {
+                alert("Has ganado!");
+            }
+            if (resultado.innerHTML.indexOf(letra) == -1) {
+                n_vidas--;
+                vidas.innerHTML = "Tienes " + n_vidas + " vidas";
+            }
+            if (n_vidas == 0) {
+                alert("Has perdido!");
+            }
+        });
+    }  
 }
